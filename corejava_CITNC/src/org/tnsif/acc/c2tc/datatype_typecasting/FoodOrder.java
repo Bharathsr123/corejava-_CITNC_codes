945e1ab4fc14b3e65123130a86f5fae2a4ae092c
bhara <bhara@BHARATHsLOQ>
2026-07-21 10:41:21 +0530

    Initial Commit

---
+++ corejava_CITNC/src/org/tnsif/acc/c2tc/datatype_typecasting/FoodOrder.java
@@ -0,0 +1,33 @@
+package org.tnsif.acc.c2tc.datatype_typecasting;
+
+public class FoodOrder {
+
+	public static void main(String[] args) {
+		int orderId = 101;
+		double price = 299.99;
+		boolean isDelivered = false;
+		char rating = 'A';
+		float deliveryDistance=5.3f;
+		long deliveryBoyPhone=9738438174L;
+		byte deliverTime=45;
+		short restaurantId=12;
+		String CusomerName="Anjali";
+		String[]foodItem= {"Burger","Fries","Coke"};
+		System.out.println("Order Summary");
+		System.out.println("Customer Name:"+CusomerName);
+		System.out.println("Order Id:"+orderId);
+		System.out.println("RestaurantID:"+restaurantId);
+		System.out.println("Food Items:");
+		for(String item : foodItem)
+		{
+			System.out.println(" - "+ item);
+		}
+		System.out.println("Total Price:"+price);
+		System.out.println("DeliveryDistance:"+deliveryDistance);
+		System.out.println("Expected Time:"+deliverTime);
+		System.out.println("Delivery Status:"+isDelivered);
+		System.out.println("Rating:"+ rating);	
+
+	}
+
+}
