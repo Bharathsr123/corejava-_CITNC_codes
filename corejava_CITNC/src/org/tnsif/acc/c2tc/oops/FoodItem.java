2e7a1d353299ceb6b5c8aee2d82fbbc7774c2dff
bhara <bhara@BHARATHsLOQ>
2026-07-22 15:24:39 +0530

    first commit

---
+++ corejava_CITNC/src/org/tnsif/acc/c2tc/oops/FoodItem.java
@@ -0,0 +1,30 @@
+package org.tnsif.acc.c2tc.oops;
+
+public class FoodItem {
+	String name;
+	double price;
+	String category;
+	void displayDetails()
+	{
+		System.out.println("Food Name:"+ name);
+		System.out.println("Price:"+price);
+		System.out.println("Category:"+category);
+		System.out.println("---");
+	}
+
+	public static void main(String[] args) {
+		FoodItem foodItem1=new FoodItem();
+		foodItem1.name="Burger";
+		foodItem1.price=299.99;
+		foodItem1.category="Fast Food";
+		foodItem1.displayDetails();
+		FoodItem foodItem2=new FoodItem();
+		foodItem2.name="Biryani";
+		foodItem2.price=399.99;
+		foodItem2.category="Rice";
+		foodItem2.displayDetails();
+
+
+	}
+
+}
